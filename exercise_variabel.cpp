4810b4eab97aae69ef7d52027ef62e6294acf85c
Dzaru Rizky Fathan Fortuna <92631060+dzarurizkyy@users.noreply.github.com>
2021-10-16 21:44:49 +0700

    4_Variabel

---
+++ exercise_variabel.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "\t ====== Introduce Myself ====== \n\n";
+    string a;
+    
+    cout << "Hello ! ";
+    cin >> a;
+
+    // Variabel
+    cout << "Let me Introduce Myself !" << endl;
+    cout << "My Name Dzaru Rizky Fathan Fortuna" << endl;
+    cout << "My age 18 years and now i live in Kediri, Jawa Timur" << endl;
+    cout << "Nice to meet you ^_^" << endl;
+
+    cin.get();
+    return 0;
+}
