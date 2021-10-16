57afebbc60da98d783b5e8b53971bb753d40a63d
Dzaru Rizky Fathan Fortuna <92631060+dzarurizkyy@users.noreply.github.com>
2021-10-16 21:37:15 +0700

    3_Printing

---
+++ exercise_printing.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Variabel
+string a;
+
+int main (){
+    cout << "\t ======= Printing Test ======= \n" << endl;
+    
+    // Printing
+    cout << "Apa warna favorit anda? ";
+    cin >> a;
+    cout << a << endl;
+
+    cin.get();
+    return 0;
+}
