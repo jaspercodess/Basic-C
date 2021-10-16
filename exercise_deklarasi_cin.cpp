d3fcc01e46005fa652e7d4c97602ebc57903e90b
Dzaru Rizky Fathan Fortuna <92631060+dzarurizkyy@users.noreply.github.com>
2021-10-16 21:46:28 +0700

    5_Deklarasi & Cin

---
+++ exercise_deklarasi_cin.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Variabel
+int a;
+
+int main () {
+   
+    cout << "Masukkan nomor angka 1-10 = ";
+    cin >> a;
+    cout << "Nomor anda adalah " << a << endl;
+
+}
